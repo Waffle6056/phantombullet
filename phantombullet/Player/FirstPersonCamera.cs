baf05223fdebd6ed888f0dc3612f31412a75d379
Waffle6056 <114373955+Waffle6056@users.noreply.github.com>
2025-06-15 12:44:08 -0500

    movement camera basic firing

---
+++ phantombullet/Player/FirstPersonCamera.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public partial class FirstPersonCamera : Camera3D
+{
+    [Export]
+    public float DPI = 1;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        Input.MouseMode = Input.MouseModeEnum.Captured;
+    }
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+
+        GlobalPosition = Player.Instance.GlobalPosition;
+    }
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+        if (@event is InputEventMouseMotion)
+            rotateCamera((@event as InputEventMouseMotion).ScreenRelative);
+
+    }
+    void rotateCamera(Vector2 motion)
+    {
+        Vector2 MouseVelo = motion * DPI * (float)(Math.PI / 180);
+
+        GlobalBasis = GlobalBasis.Rotated(Vector3.Up, -(float)(MouseVelo.X));
+
+        Player.Instance.GlobalBasis = Player.Instance.GlobalBasis.Rotated(Vector3.Up, -(float)(MouseVelo.X));
+
+        float YDelta = -(float)(MouseVelo.Y);
+        YDelta = Math.Clamp(YDelta, -(-GlobalBasis[2]).AngleTo(Vector3.Down) + .1f, (-GlobalBasis[2]).AngleTo(Vector3.Up) - .1f);
+        //GD.Print(YDelta);
+        GlobalBasis = GlobalBasis.Rotated(GlobalBasis[0].Normalized(), YDelta);
+    }
+}
---
+++ phantombullet/Player/Player.cs
@@ -0,0 +1,51 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody3D
+{
+	[Export]
+	public float Speed = 5.0f;
+    [Export]
+    public float JumpVelocity = 4.5f;
+	public static Player Instance;
+	public override void _Ready()
+	{
+		base._Ready();
+		Instance = this;
+	}
+
+	public override void _PhysicsProcess(double delta)//this is the builtin template btw
+	{
+		Vector3 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("Jump") && IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+		}
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 inputDir = Input.GetVector("Left", "Right", "Up", "Down");
+		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		if (direction != Vector3.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Z = direction.Z * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+		}
+		//GD.Print(velocity);
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
