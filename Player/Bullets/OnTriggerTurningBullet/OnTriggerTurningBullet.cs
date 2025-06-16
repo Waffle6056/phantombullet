1d978830f4358ef997f785af54a20d443ed3e3a5
Waffle6056 <114373955+Waffle6056@users.noreply.github.com>
2025-06-16 17:23:24 -0500

    bullet trajectory visual
    displays a dotted line where the bullet would go used for displaying the turning bullet trajectory currently

--- Player/Bullets/OnTriggerTurningBullet/OnTriggerTurningBullet.cs
+++ Player/Bullets/OnTriggerTurningBullet/OnTriggerTurningBullet.cs
@@ -7,6 +7,8 @@ public partial class OnTriggerTurningBullet : Bullet
 	public Vector3 Axis;
     [Export]
     public float AngleDegrees;
+	[Export]
+	public TrajectoryIndicator TrajectoryIndicator;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -16,16 +18,19 @@ public partial class OnTriggerTurningBullet : Bullet
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		
 	}
     public override void Fired(Gun gun)
     {
         base.Fired(gun);
 		gun.Connect(Gun.SignalName.SuccessfulFire, Callable.From(Turn));
+        TrajectoryIndicator.GlobalBasis = GlobalBasis.Rotated(Axis.Normalized(), (float)(AngleDegrees / 180 * Math.PI));
     }
 	void Turn()
 	{
-		GD.Print(GlobalRotation);
+		//GD.Print(GlobalRotation);
 		GlobalBasis = GlobalBasis.Rotated(Axis.Normalized(), (float)(AngleDegrees / 180 * Math.PI));
-        GD.Print(GlobalRotation);
+        TrajectoryIndicator.GlobalBasis = GlobalBasis.Rotated(Axis.Normalized(), (float)(AngleDegrees / 180 * Math.PI));
+        //GD.Print(GlobalRotation);
     }
 }
---
+++ Player/Bullets/TrajectoryIndicator.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+public partial class TrajectoryIndicator : Node3D
+{
+	[Export]
+	public Node3D Visuals;
+    [Export]
+    public RayCast3D Raycast;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		Vector3 start = GlobalPosition;
+		Vector3 end =  Raycast.GlobalTransform * Raycast.TargetPosition;
+		if (Raycast.IsColliding())
+			end = Raycast.GetCollisionPoint();
+
+		//GD.Print(start + " " + end);
+
+
+		Visuals.GlobalPosition = (start + end) / 2;
+		Visuals.Scale = new Vector3(1,(end - start).Length(),1);
+    }
+}
