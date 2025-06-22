f78ee9b294be618bd515cda51f38356aea76cdde
acemavrick <116685696+acemavrick@users.noreply.github.com>
2025-06-22 14:18:04 -0500

    Added pause menu

--- Main/Menu.cs
+++ Main/Menu.cs
@@ -3,9 +3,19 @@ using System;
 
 public partial class Menu : Control
 {
+    [Export]
+    public Label StageLabel;
+
+    [Export]
+    public Node LevelManager;
+
     public override void _Ready()
     {
         Hide();
+        if (StageLabel == null)
+        {
+            GD.PrintErr("Menu's StageLabel is not set. Please set it in the inspector.");
+        }
     }
 
     public override void _Input(InputEvent @event)
@@ -13,16 +23,27 @@ public partial class Menu : Control
         if (@event.IsActionPressed("Pause"))
         {
             GetViewport().SetInputAsHandled();
-            GD.Print("Pause action triggered (menu)");
-            Hide();
-            GetTree().Root.ContentScaleMode = Window.ContentScaleModeEnum.Disabled;
-            Input.MouseMode = Input.MouseModeEnum.Captured;
-            GetTree().Paused = false;
+            ResumeGame();
         }
+    }
+
+    public void ResumeGame()
+    {
+        GD.Print("Resuming game...");
+        Hide();
+        GetTree().Root.ContentScaleMode = Window.ContentScaleModeEnum.Disabled;
+        Input.MouseMode = Input.MouseModeEnum.Captured;
+        GetTree().Paused = false;
+    }
+
+    public void QuitToMain()
+    {
+        GD.Print("Quit to main menu... unimplemented");
     }
 
     public override void _Process(double delta)
     {
         Visible = true;
+        StageLabel.Text = $"{(LevelManager as LevelManager).CurrentLevel.Name}";
     }
 }
